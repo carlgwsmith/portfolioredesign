35784071351f64b02a41257babf402d0204a7dc1
carlvskansas <48308402+carlgwsmith@users.noreply.github.com>
2021-08-15 21:59:57 -0500

    rollerover and images

--- src/Components/Projects/ProjectCard.js
+++ src/Components/Projects/ProjectCard.js
@@ -1,8 +1,12 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import { ProjCard, ProjTitle } from './ProjectElements';
+import { useHistory } from "react-router-dom";
 
 const ProjectCard = (props) => {
 const [hover, setHover] = useState(false)
+const [projUrl, setProjUrl] = useState('')
+
+let history = useHistory();
 
 const handleMouseEnter = () => {
   setHover(true);
@@ -10,9 +14,15 @@ const handleMouseEnter = () => {
 const handleMouseLeave = () => {
   setHover(false);
 }
+function handleClick() {
+  history.push(projUrl);
+}
 
+useEffect(() => {
+  setProjUrl(props.url)
+}, [props.url]);
   return (
-    <ProjCard style={{background: hover ? "linear-gradient(rgba("+props.rgb+", 0.6), rgba("+ props.rgb +", 0.6)),url(" + props.image + ")" : "url(" + props.image + ")"}} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+    <ProjCard style={{background: hover ? "linear-gradient(rgba("+props.rgb+", 0.6), rgba("+ props.rgb +", 0.6)),url(" + props.image + ")" : "url(" + props.image + ")"}} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} onClick={handleClick}>
      <ProjTitle>
        <h1>{props.title}</h1>
        <p>Case Study</p>
--- src/Pages/Projects.js
+++ src/Pages/Projects.js
@@ -1,32 +1,16 @@
 import React from 'react';
 import { ResumeContainer, SideA1, SideA2, SideB1, SideB2 } from '../Components/Projects/ProjectElements';
 import Card from '../Components/Projects/ProjectCard'
-import { useHistory } from 'react-router-dom'
 
 const Projects = () => {
 
-  const history = useHistory();
-
-  // function FMCase (){
-  //   history.push('/projects/fomomachine')
-  // }
-  // function RCCase (){
-  //   history.push('/projects/retirementcalc')
-  // }
-  // function CCCase (){
-  //   history.push('/projects/cryptocompare')
-  //   console.log('go')
-  // }
-  // function ISCase (){
-  //   history.push('/projects/incomesolver')
-  // }
   return (
     <div>
       <ResumeContainer>
-        <SideA1><Card title="Crypto Compare" rgb="64,94,104"image={process.env.PUBLIC_URL + "/images/cryptoCompare.png"} onClick={() => history.push('/projects/cryptocompare')}/></SideA1>
-        <SideB1><Card title="FOMO Machine" rgb="64,94,104"image={process.env.PUBLIC_URL + "/images/cryptoCompare.png"} onClick={() => history.push('/projects/fomomachine')}/></SideB1>
-        <SideA2><Card title="Retirement Calculator" rgb="64,94,104"image={process.env.PUBLIC_URL + "/images/cryptoCompare.png"} onClick={() => history.push('/projects/retirementcalc')}/></SideA2>
-        <SideB2><Card title="Income Solver" rgb="01,116,254"image={process.env.PUBLIC_URL + "/images/solverLogo.png"} onClick={() => history.push('/projects/incomesolver')}/></SideB2>
+        <SideA1><Card title="Crypto Compare" rgb="64,94,104" image={process.env.PUBLIC_URL + "/images/CryptoCompare.jpg"} url="/projects/cryptocompare"/></SideA1>
+        <SideB1><Card title="FOMO Machine" rgb="106,107,255" image={process.env.PUBLIC_URL + "/images/FOMOMachine.jpg"} url="/projects/fomomachine"/></SideB1>
+        <SideA2><Card title="Retirement Calculator" rgb="39, 76, 133" image={process.env.PUBLIC_URL + "/images/retirementcalc.jpg"} url="/projects/retirementcalc"/></SideA2>
+        <SideB2><Card title="Income Solver" rgb="01,116,254" image={process.env.PUBLIC_URL + "/images/solverLogo.png"} url="/projects/incomesolver"/></SideB2>
       </ResumeContainer>
     </div>
   );
