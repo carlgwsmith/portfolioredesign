87b034246b02f191ff0ffa7717cf81e23ce16141
carlvskansas <48308402+carlgwsmith@users.noreply.github.com>
2021-10-13 22:29:29 -0500

    Updates

---
+++ precache-manifest.720736c04bbd57fd12e44760baacd7d6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48823c7d75729a877c75c12f871cbd76",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d42ee716f15cf1786459",
+    "url": "/static/css/2.9522b9fe.chunk.css"
+  },
+  {
+    "revision": "0ec9c65280773e606a07",
+    "url": "/static/css/main.e574f0cb.chunk.css"
+  },
+  {
+    "revision": "d42ee716f15cf1786459",
+    "url": "/static/js/2.b2c87cba.chunk.js"
+  },
+  {
+    "revision": "bcf51325c19c950646adef1ab31d91ae",
+    "url": "/static/js/2.b2c87cba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5900b0be2c78001f172f",
+    "url": "/static/js/3.18cfa952.chunk.js"
+  },
+  {
+    "revision": "0ec9c65280773e606a07",
+    "url": "/static/js/main.772fc8e6.chunk.js"
+  },
+  {
+    "revision": "1668930747e810392c7d",
+    "url": "/static/js/runtime-main.2324d28b.js"
+  }
+]);
